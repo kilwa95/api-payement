17a64d9e1303d6093020f42779be91bf5912ab77
khaled <lga.sig.khaled@gmail.com>
2021-06-13 00:17:27 +0200

    article-composant-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-function App() {
-	return (
-		<div className="App">
-			<h1>hello world</h1>
-		</div>
-	);
+import React, { Component } from 'react';
+import Article from '../src/UI/article/Article';
+
+class App extends Component {
+	render() {
+		return <Article title="montre" description="montre made in suisse" date="2015-2022" prix={200} />;
+	}
 }
 
 export default App;
