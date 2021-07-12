b617eab5f0ea35efbe4a48804ec400ce414acbfa
khaled <lga.sig.khaled@gmail.com>
2021-07-12 22:06:00 +0200

    CredentialsProvider

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,23 @@ import React, { Component } from 'react';
 import HomePage from './views/homepage/HomePage';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import ListProvider from "./contexts/ListContext";
+import CredentialsProvider from "./contexts/CredentialContext";
 
 class App extends Component {
 	render() {
 		return (
-			<HashRouter>
-				<React.Suspense>
-					<Switch>
-					<ListProvider>
-						<Route exact path="/" name="HomePage" render={(props) => <HomePage {...props} />} />
-					</ListProvider>				
-					</Switch>
-				</React.Suspense>
-			</HashRouter>
+			<CredentialsProvider>
+				<HashRouter>
+					<React.Suspense>
+						<Switch>
+						<ListProvider>
+							<Route exact path="/" name="HomePage" render={(props) => <HomePage {...props} />} />
+						</ListProvider>				
+						</Switch>
+					</React.Suspense>
+				</HashRouter>
+			</CredentialsProvider>
+
 		);
 	}
 }
