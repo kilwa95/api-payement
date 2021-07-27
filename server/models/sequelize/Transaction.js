6ded448b7aa09876315f1ca69dd229d6b9b273ef
aza93 <a.azabar93@gmail.com>
2021-07-27 17:55:33 +0200

    Transaction

--- server/models/sequelize/Transaction.js
+++ server/models/sequelize/Transaction.js
@@ -1,34 +1,30 @@
 const { Model, DataTypes } = require('sequelize');
 const connection = require('../../config/sequelize');
 const User = require('./User');
-const Address = require('./Address');
-const Panier = require('./Panier');
-
 
 class Transaction extends Model {}
 
 Transaction.init(
-	{
-        priceTotal: DataTypes.INTEGER,
-        factorisation: DataTypes.STRING,
-        quote: DataTypes.STRING
-	},
-	{
-		sequelize: connection,
-		modelName: 'Transaction'
-	}
+  {
+    priceTotal: DataTypes.INTEGER,
+    delivery: DataTypes.STRING,
+    status: DataTypes.STRING,
+    quote: DataTypes.STRING,
+    clientInformation: DataTypes.STRING,
+    panierUser: DataTypes.STRING,
+    commandId: DataTypes.STRING,
+  },
+  {
+    sequelize: connection,
+    modelName: 'Transaction',
+  }
 );
 
-
-Transaction.belongsTo(User, { as: 'user' });
-Transaction.belongsTo(Address, { as: 'delivery' });
-User.hasMany(Transaction, { foreignKey: 'userId', as: 'transactions' });
-Address.hasMany(Transaction, { foreignKey: 'addressId', as: 'transactions' });
-
-
+// Transaction.belongsTo(User, { as: 'user' });
+// User.hasMany(Transaction, { foreignKey: 'userId', as: 'transactions' });
 
 Transaction.sync({
-	alter: true
+  alter: true,
 });
 
 module.exports = Transaction;
