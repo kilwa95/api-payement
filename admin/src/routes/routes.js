31e69a1008b41ff06fa3d88b775518ed16668b5c
aza93 <a.azabar93@gmail.com>
2021-07-27 17:55:46 +0200

    Routes

--- admin/src/routes/routes.js
+++ admin/src/routes/routes.js
@@ -4,19 +4,21 @@ import MerchantProvider from '../contexts/MerchantContext'
 import TransactionProvider from '../contexts/TransactionContext'
 
 const routes = [
-	{ path: '/admin', exact: true, name: 'admin' },
-	{
-		path: '/admin/merchants',
-		name: 'merchants',
-		provider: MerchantProvider,
-		component: MerchantPage
-	},
-	{
-		path: '/admin/transactions',
-		name: 'transactions',
-		provider: TransactionProvider,
-		component: TransactionPage
-	}
+    { path: '/admin', exact: true, name: 'admin' },
+    {
+        exact: true,
+        path: '/admin/merchants',
+        name: 'merchants',
+        provider: MerchantProvider,
+        component: MerchantPage
+    },
+    {
+        exact: true,
+        path: '/admin/transactions',
+        name: 'transactions',
+        provider: TransactionProvider,
+        component: TransactionPage
+    }
 ];
 
 export default routes;
