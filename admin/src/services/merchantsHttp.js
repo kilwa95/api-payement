83343510e79f4101796b2e7926339b6750ff4fd7
aza93 <a.azabar93@gmail.com>
2021-07-27 17:57:18 +0200

    Change id path (marchant)

--- admin/src/services/merchantsHttp.js
+++ admin/src/services/merchantsHttp.js
@@ -1,22 +1,22 @@
 import * as axios from 'axios';
 
 const merchantsHttp = {
-	fetchMerchants: async function() {
-		const merchants = await axios.get('http://localhost:3001/admin/merchants', {
-			headers: {
-				Authorization: `Bearer ${localStorage.getItem('token')}`
-			}
-		});
-		return merchants;
-	},
-	updateMerchants: async function(id, body) {
-		const merchant = await axios.put(`http://localhost:3001/admin/merchants/valid/${id}`, body, {
-			headers: {
-				Authorization: `Bearer ${localStorage.getItem('token')}`
-			}
-		});
-		return merchant;
-	}
+    fetchMerchants: async function() {
+        const merchants = await axios.get('http://localhost:4000/merchants', {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
+        });
+        return merchants;
+    },
+    updateMerchants: async function(id, body) {
+        const merchant = await axios.put(`http://localhost:4000/merchants/${id}/validate`, body, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
+        });
+        return merchant;
+    }
 };
 
 export default merchantsHttp;
