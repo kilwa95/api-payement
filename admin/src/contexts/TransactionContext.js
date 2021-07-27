0027a197b10b343e5d93e8575d0ffcf4316233aa
Eddie Saliby <eddysaliby@gmail.com>
2021-07-27 14:10:22 +0200

    get transaction infos

--- admin/src/contexts/TransactionContext.js
+++ admin/src/contexts/TransactionContext.js
@@ -1,50 +1,56 @@
 import {
-    createContext,
-    useState,
-    useEffect,
-    useMemo,
-    useCallback,
-  } from "react";
+  createContext,
+  useState,
+  useEffect,
+  useMemo,
+  useCallback,
+} from "react";
 
-  import transactionHttp from '../services/transactionHttp'
-
-
-  export const TransactionContext = createContext();
-
-  export default function TransactionProvider({ children }) {
-    const [ transactions, setTransactions ] = useState([]);
+import transactionHttp from '../services/transactionHttp'
 
 
-    const fields = [
-      { key: 'priceTotal' },
-      { key: 'userFirstName' },
-      { key: 'userLastName' },
-      { key: 'deliveryStreet' },
-      { key: 'deliveryCity' },
-      { key: 'deliveryPostalCode' },
-      { key: 'produitsInCart' },
-    ];
+export const TransactionContext = createContext();
+
+export default function TransactionProvider({ children }) {
+  const [ transactions, setTransactions ] = useState([]);
 
 
-    useEffect(() => {
-        async function fetchData() {
-            const transactions = await transactionHttp.fetchTransactions();
-            setTransactions(transactions.data.data.transactions);
-        }
-		fetchData();
-	}, []);
+  const fields = [
+    { key: 'priceTotal' },
+    { key: 'userFirstName' },
+    { key: 'userLastName' },
+    { key: 'deliveryStreet' },
+    { key: 'deliveryCity' },
+    { key: 'deliveryPostalCode' },
+    { key: 'produitsInCart' },
+  ];
 
 
-    return (
-        <TransactionContext.Provider
-          value={{
-            transactions,
-            fields
-          }} 
-        >
-          {children}
-        </TransactionContext.Provider>
-      );
-  }
+  useEffect(() => {
+      async function fetchData() {
+          const transactions = await transactionHttp.fetchTransactions();
+          setTransactions(transactions.data.data.transactions);
+      }
+      fetchData();
+  }, []);
+
+  useEffect(() => {
+      async function fetchData() {
+          const transactions = await transactionHttp.fetchTransactions();
+          setTransactions(transactions.data.data.transactions);
+      }
+      fetchData();
+  }, []);
 
 
+  return (
+      <TransactionContext.Provider
+        value={{
+          transactions,
+          fields
+        }} 
+      >
+        {children}
+      </TransactionContext.Provider>
+    );
+}
