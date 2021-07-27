f2ef6c2928d392d61b555d34066cdef2618d4417
Eddie Saliby <eddysaliby@gmail.com>
2021-07-27 14:08:23 +0200

    Add Field data table for marchant

--- admin/src/contexts/MerchantContext.js
+++ admin/src/contexts/MerchantContext.js
@@ -1,75 +1,75 @@
 import {
-    createContext,
-    useState,
-    useEffect,
-    useMemo,
-    useCallback,
-  } from "react";
+  createContext,
+  useState,
+  useEffect,
+  useMemo,
+  useCallback,
+} from "react";
 
 import merchantsHttp from '../services/merchantsHttp'
 
-  export const MerchantContext = createContext();
+export const MerchantContext = createContext();
 
-  export default function MerchantProvider({ children }) {
-    const [ merchants, setMerchants ] = useState([]);
+export default function MerchantProvider({ children }) {
+  const [ merchants, setMerchants ] = useState([]);
 
-    const fields = [
-      {
-        key: 'activate_company',
-        label: '',
-        sorter: false,
-        filter: false
+  const fields = [
+    {
+      key: 'activate_company',
+      label: '',
+      sorter: false,
+      filter: false
+    },
+    { key: 'companyName' },
+    { key: 'valid' },
+    { key: 'email' },
+    { key: 'kbis' },
+    { key: 'quote' },
+    { key: 'site' },
+    { key: 'urlAnnulation' },
+    { key: 'urlConfirmation' }
+  ];
+
+  useEffect(() => {
+      async function fetchData() {
+          const merchants = await merchantsHttp.fetchMerchants();
+          setMerchants(merchants.data.data.merchants);
+      }
+      fetchData();
+  }, []);
+
+
+  const switchToggle = useCallback(
+      async (item) => {
+          let merchant;
+          if (item && item.valid) {
+              merchant = await merchantsHttp.updateMerchants(item.id, { valid: false });
+              setMerchants(
+                  merchants.map((m) => (m.id !== merchant.data.data.merchant.id ? m : merchant.data.data.merchant))
+              );
+          } else {
+              merchant = await merchantsHttp.updateMerchants(item.id, { valid: true });
+
+              setMerchants(
+                  merchants.map((m) => (m.id !== merchant.data.data.merchant.id ? m : merchant.data.data.merchant))
+              );
+          }
       },
-      { key: 'companyName' },
-      { key: 'email' },
-      { key: 'phone' },
-      { key: 'valid' },
-      { key: 'city' },
-      { key: 'postalCode' },
-      { key: 'street' }
-    ];
-
-    useEffect(() => {
-        async function fetchData() {
-            const merchants = await merchantsHttp.fetchMerchants();
-            setMerchants(merchants.data.data.merchants);
-        }
-		fetchData();
-	}, []);
-    
-
-    const switchToggle = useCallback(
-        async (item) => {
-            let merchant;
-            if (item && item.valid) {
-                merchant = await merchantsHttp.updateMerchants(item.id, { valid: false });
-                setMerchants(
-                    merchants.map((m) => (m.id !== merchant.data.data.merchant.id ? m : merchant.data.data.merchant))
-                );
-            } else {
-                merchant = await merchantsHttp.updateMerchants(item.id, { valid: true });
-    
-                setMerchants(
-                    merchants.map((m) => (m.id !== merchant.data.data.merchant.id ? m : merchant.data.data.merchant))
-                );
-            }
-        },
-        [merchants]
-      );
+      [merchants]
+    );
 
 
-    return (
-        <MerchantContext.Provider
-          value={{
-            merchants,
-            fields,
-            switchToggle
-          }} 
-        >
-          {children}
-        </MerchantContext.Provider>
-      );
+  return (
+      <MerchantContext.Provider
+        value={{
+          merchants,
+          fields,
+          switchToggle
+        }} 
+      >
+        {children}
+      </MerchantContext.Provider>
+    );
 
 
-  }
-
+}
