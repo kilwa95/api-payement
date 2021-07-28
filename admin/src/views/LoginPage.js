793aadb5b6ce71050eb90b59df28b0f429931e2b
Khaled Abdulhalim <lga.sig.khaled@gmail.com>
2021-07-28 18:57:45 +0200

    update token

--- admin/src/views/LoginPage.js
+++ admin/src/views/LoginPage.js
@@ -4,30 +4,38 @@ import usersHttp from '../services/usersHttp';
 import useToken from '../hooks/useToken';
 import { Redirect } from 'react-router-dom';
 
+function getToken(resultat) {
+  let token = '';
 
+  const authorization = resultat.headers.authorization;
+  if (!!authorization && authorization.startsWith('Bearer ')) {
+    token = authorization.slice(7, authorization.length);
+  }
+  return token;
+}
 
 const LoginPage = ({ setToken, setUser }) => {
-	const [ error, setError ] = useState('');
-	const {token} = useToken();
+  const [error, setError] = useState('');
+  const { token } = useToken();
 
-	const onSubmit = async (values) => {
-		const data = await usersHttp.loginUser(values);
-		let { user, token } = data;
-		if (user) {
-			setError('');
-			setToken(token);
-			setUser(user);
-		}
-		else{
-			setError('le user not found ou le mot passe incorrect');
-		}
-	};
+  const onSubmit = async values => {
+    const data = await usersHttp.loginUser(values);
+    let { user } = data;
+    const token = getToken(data);
+    if (user) {
+      setError('');
+      setToken(token);
+      setUser(user);
+    } else {
+      setError('le user not found ou le mot passe incorrect');
+    }
+  };
 
-	if(token){
-		return <Redirect to="/" />
-	}
+  if (token) {
+    return <Redirect to="/" />;
+  }
 
-	return <LoginForm error={error} onSubmit={onSubmit} />;
+  return <LoginForm error={error} onSubmit={onSubmit} />;
 };
 
 export default LoginPage;
